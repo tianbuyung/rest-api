f4bc52568527b3df71cbc7a5c60d5e91ba5a3900
Septian Maulana <septianm028@gmail.com>
2023-11-18 21:47:27 +0700

    feat: define users controller for crud method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // GET /users
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  // GET /users/interns
+  @Get('interns')
+  findAllInterns() {
+    return [];
+  }
+
+  // GET /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /users
+  @Post()
+  create(@Body() user: any) {
+    return user;
+  }
+
+  // PATCH /users/:id
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: any) {
+    return { id, ...userUpdate };
+  }
+
+  // DELETE /users/:id
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
