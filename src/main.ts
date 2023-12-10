968cb762b241143437c4c562ac77107878434d28
Septian Maulana <septiandev028@gmail.com>
2023-12-10 14:06:42 +0700

    feat: enable cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { corsOptions } from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
