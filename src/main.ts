f4925cbc4730811147ec859c2f82b8cddb02acc5
Septian Maulana <septiandev028@gmail.com>
2023-12-10 22:16:55 +0700

    chore: update main.ts to add app-logger service as global

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsOptions } from 'config';
+import { AppLoggerService } from './app-logger/app-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(AppLoggerService));
   app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
   await app.listen(3000);
